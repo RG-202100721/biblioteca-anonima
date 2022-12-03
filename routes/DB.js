b29cf7f4e14ba023ac488736ca0912725f1ec289
RG-202100721 <202100721@estudantes.ips.pt>
2022-12-03 16:09:15 +0000

    DB connection

---
+++ routes/DB.js
@@ -0,0 +1,81 @@
+const fs = require("fs");
+const mysql = require('mysql');
+
+//criação da conexão ao servidor mysql
+exports.start = () => {
+    dbconfig = {
+        //credenciais do servidor mysql
+        connectionLimit: 100,
+        host: "remotemysql.com",
+        port: 3306,
+        database: "yOVZXrSpNL",
+        user: "yOVZXrSpNL",
+        password: "MvHQ1igl7N",
+        multipleStatements: true
+    };
+
+    pool = mysql.createPool(dbconfig);
+    pool.getConnection((err, con) => {
+		if (err) console.log('Error in DB connection (start): ' + err);
+        console.log("Connected!");
+        con.release();
+	});
+};
+
+//criação da base de dados a partir do ficheiro database.sql
+exports.create = () => {
+    var sql = fs.readFile('./scripts/database.sql').toString().replace(/\s+/g, ' ').split("\r\n").join('');
+    pool.getConnection((err, con) => {
+		if (err) {
+			con.release();
+	  		console.log('Error in DB connection (create): ' + err);
+	  	}
+        else {
+            con.query(sql, (err2, result) => {
+                if (err) console.log('Error in DB query (create): ' + err2);
+                console.log("Database created!");
+                con.release();
+            });
+        }
+	});
+};
+
+//faz uma query à base de dados (select, update, insert and delete)
+exports.query = (sql, callback) => {
+    pool.getConnection((err, con) => {
+		if (err) {
+			con.release();
+	  		console.log('Error in DB connection (query): ' + err);
+	  	}
+        else {
+            con.query(sql, (err2, result) => {
+                if (err) console.log('Error in DB query (query): ' + err2);
+                if (JSON.stringify(sql).includes("INSERT")) {
+                    console.log(`Database row inserted! [Query: ${sql}]`);
+                    callback(null, result);
+                }
+                else if (JSON.stringify(sql).includes("UPDATE")) {
+                    console.log(`Database row updated! [Query: ${sql}]`);
+                    callback(null, null);
+                }
+                else if (JSON.stringify(sql).includes("DELETE")) {
+                    console.log(`Database row deleted! [Query: ${sql}]`);
+                    callback(null, null);
+                }
+                else {
+                    console.log(`Database queried! [Query: ${sql}]`);
+                    callback(null, result);
+                }
+                con.release();
+            });
+        }
+	});
+};
+
+//fim da conexão ao servidor mysql
+exports.end = () => {
+    pool.end((err) => {
+        if (err) console.log('Error in DB connection (end): ' + err);
+        console.log("Connection closed!");
+    });
+};
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const routes = require("./routes/routes");
+const DB = require("./routes/DB");
 const PORT = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use(express.static(path.resolve("www")));
 
-app.get("/", () => {
-    
-});
+
 
 app.listen(PORT, () => {
     console.log("Server is on port: " + PORT);
