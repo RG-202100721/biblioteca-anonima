decb26795797064ab19ad8f9db2f0c0021de627f
RG-202100721 <202100721@estudantes.ips.pt>
2022-12-03 20:38:34 +0000

    Updated routes

--- routes/routes.js
+++ routes/routes.js
@@ -7,22 +7,59 @@ const DB = require("./DB");
 
 //rotas de interação com a base de dados
 router.get("/getData", (req, res) => {
-    
+    var sql = `SELECT * FROM ${req.query.table};`;
+
+    DB.query(sql, (err, result) => {
+   		if (err) throw err;
+   		if (result) res.json({ data: result });
+        else res.json({ data: '0 results.' });
+	});
 });
 router.post("/create", (req, res) => {
-
+    var sql = `INSERT INTO ${req.body["Tabela"]} `;
+    switch (req.body["Tabela"]) {
+        case "Autor": 
+            sql += `(Nome, Pais) VALUES ('${req.body["Nome"]}', '${req.body["Pais"]}');`;
+            break;
+    }
+            
+    dbcon.query(sql, (err, result) => {
+       	if (err) { res.json({ data: '0 results.' }); throw err; }
+        else res.json({ message: `Database row inserted! [Query: ${sql}]` });
+	});
 });
 router.put("/edit", (req, res) => {
+    var sql = `UPDATE ${req.body["Tabela"]} SET `;
+    switch (req.body["Tabela"]) {
+        case "Autor": 
+            sql += `Nome = '${req.body["Nome"]}', Pais = '${req.body["Pais"]}'`;
+            break;
+    }
+    sql += ` WHERE ID = ${req.body["ID"]};`
 
+    dbcon.query(sql, (err, result) => {
+   		if (err) { res.json({ data: '0 results.' }); throw err; }
+        else res.json({ message: `Database row updated! [Query: ${sql}]` });
+	});
 });
-router.post("/deleteData", (req, res) => {
+router.post("/delete", (req, res) => {
+    var sql = '';
+    switch (req.body["Tabela"]) {
+        case "Autor":
+            sql = `DELETE FROM ${req.body["Tabela"]} WHERE ID = ${req.body["ID"]};`;
+            break;
+    }
 
+    dbcon.query(sql, (err, result) => {
+        if (err) { res.json({ data: '0 results.' }); throw err; }
+        else res.json({ message: `Database row deleted! [Query: ${sql}]` });
+    });
 });
 
 
 //erro 404
 router.get('*', (req, res) => {       
-    res.json('Erro, URL inválido.');
+    res.json({ data: 'Erro, URL inválido.' });
 });
 
 
